d36f936fe3259aea6c3688ad2b3a289f34c00c05
Dokkabei97 <wkdrn970@naver.com>
2024-04-12 17:40:51 +0900

    11

--- tests/yaml_test.go
+++ tests/yaml_test.go
@@ -1,8 +1,8 @@
 package tests
 
 import (
+	"gopkg.in/yaml.v3"
 	"os"
-	"path/filepath"
 	"testing"
 )
 
@@ -19,25 +19,29 @@ type TestMonitoring struct {
 
 type TestPolicy struct {
 	Usage          int                `yaml:"usage"`
-	PathFileformat TestPathFileformat `yaml:"path-fileformat"`
+	PathFileFormat TestPathFileFormat `yaml:"path-file-format"`
 	Time           string             `yaml:"time"`
 	Auto           bool               `yaml:"auto"`
 }
 
-type TestPathFileformat struct {
+type TestPathFileFormat struct {
 	Include []string `yaml:"include"`
 	Exclude []string `yaml:"exclude"`
 }
 
-func TestRemoveFile(t *testing.T) {
-	//file, _ := os.ReadFile("./test.yaml")
-	//
-	//var config TestConfig
-	//_ = yaml.Unmarshal(file, &config)
-}
+func TestYaml(t *testing.T) {
+	// Given
+	file, _ := os.ReadFile("./test.yaml")
 
-func removeFile(path string, excludePath []string) error {
-	return filepath.Walk(path, func(file string, info os.FileInfo, err error) error {
+	var config TestConfig
+	err := yaml.Unmarshal(file, &config)
 
-	})
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	// When Then
+	if config.Monitoring.AlertUsage != 80 {
+		t.Errorf("Expected 80, got %d", config.Monitoring.AlertUsage)
+	}
 }
